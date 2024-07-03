2fbbdc7a29408c6da2ea71cb718d027f111c1ca3
KirtiYadav1681 <yadavkirti7745@gmail.com>
2024-07-03 18:32:28 +0530

    changed url

--- app/lib/fetchBlogs.js
+++ app/lib/fetchBlogs.js
@@ -1,5 +1,5 @@
 export async function fetchBlogs() {
-  const res = await fetch("http://localhost:3000/api/all-blogs");
+  const res = await fetch("http://127.0.0.1:3000/api/all-blogs");
   if (!res.ok) {
     throw new Error("Featured blog not found.");
   }
